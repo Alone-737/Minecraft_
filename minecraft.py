c64c79d5013ffd09d045ca224dc30885a2ffe9ab
Alone-737 <yogeshdangal66@gmail.com>
2025-04-15 20:05:48 +0545

    Update minecraft.py

--- minecraft.py
+++ minecraft.py
@@ -2,31 +2,52 @@ from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
+
 player = FirstPersonController()
-Sky()
 
 sword_texture = load_texture("sword.png")
+Sky()
 
 sword = Entity(
     parent=camera.ui,
     model="cube",
     texture=sword_texture,
     position=(0.6, -0.4),
-    scale=(0.3, 0.5),
+    scale=(1, 1, 1),
+    enabled=False,
+    rotation=(0, 0, -90)
 )
+
+
+attack_in_progress = False
+
+
+def attack():
+    global attack_in_progress
+    if attack_in_progress:
+        return
+    attack_in_progress = True
+    sword.enabled = True
+    sword.animate_rotation_z(45, duration=0.1)
+    sword.animate_rotation_z(-90, duration=0.1, delay=0.1)
+    invoke(finish_attack, delay=0.2)
+
+
+def finish_attack():
+    global attack_in_progress
+    sword.enabled = False
+    attack_in_progress = False
+
+
+sword.on_click = attack
 
 
 def update():
     if held_keys["left mouse"] or held_keys["right mouse"]:
-        sword.position = (0.5, -0.3)
-        sword.scale = (0.6, 0.9)
-    else:
-        sword.position = (0.6, -0.4)
-        sword.scale = (0.5, 0.8)
+        attack()
 
 
 boxes = []
-
 for i in range(20):
     for j in range(20):
         box = Button(
